94335fd8814d2064a16c4b7aea7eff9ca5d5c377
mosab-gaafow <devmuscab@gmail.com>
2025-04-19 22:04:07 +0300

    Added CreatedAt in the response.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -57,6 +57,7 @@ router.post('/register', async(req, res) => {
                 username: user.username,
                 email: user.email,
                 profileImage: user.profileImage,
+                createdAt: user.createdAt,
             }
         })
 
@@ -97,6 +98,7 @@ router.post('/login', async(req, res) => {
                 username: user.username,
                 email: user.email,
                 profileImage: user.profileImage,
+                createdAt: user.createdAt,
             }
         })
 
