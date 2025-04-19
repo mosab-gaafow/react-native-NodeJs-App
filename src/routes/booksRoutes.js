ff743ee982bad79261459591185e8bbcc463592d
mosab-gaafow <devmuscab@gmail.com>
2025-04-19 19:10:48 +0300

    fixed user path required on books route

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -23,7 +23,7 @@ router.post('/', protectRoute, async (req, res) => {
             caption,
             rating,
             image: imageUrl,
-            // user: req.user._id
+            user: req.user._id
         });
 
         await newBook.save();
